8f34dc9cf30cab43a812a3a3b708f588e81ce029
Geoffrey Walters <geoffrey.walters@noaa.gov>
2021-12-18 12:50:23 -0800

    Update to SNOW17 FORTRAN Code

--- py-rfchydromodels/nwsrfs_models/aeco19.f
+++ py-rfchydromodels/nwsrfs_models/aeco19.f
@@ -9,20 +9,35 @@ C        USED IN THE 'SNOW-17' OPERATION.
 C.......................................
 C     SUBROUTINE INITIALLY WRITTEN BY...
 C        ERIC ANDERSON-HRL APRIL 1981
-
+C
 CVK      MODIFIED 4/00 BY V. KOREN: NEW STATES, SNDPT & SNTMP ADDED
+C
+CEA      MODIFIED 11/05 BY E. ANDERSON TO ADD TAPREV TO SNCO19 COMMON
+CEA        - NO OTHER CHANGES NEEDED.
+C
+CEA      MODIFIED 1/06 BY E. ANDERSON FOR MODIFICATIONS TO HOW NEW
+CEA         SNOWFALL VALUES BELOW SNOF ARE HANDLED.
 C.......................................
       REAL NEGHS,LIQW
       DIMENSION ADC(11)
 C
 C     COMMON BLOCK
+CVK   ADDED TWO MORE STATES
       COMMON/SNCO19/WE,NEGHS,LIQW,TINDEX,ACCMAX,SB,SBAESC,SBWS,STORGE,
-
-CVK  ADDED TWO MORE STATES
 CVK     1AEADJ,NEXLAG,EXLAG(7)
-     1AEADJ,NEXLAG,EXLAG(7),SNDPT,SNTMP
-     
+CEA     1AEADJ,NEXLAG,EXLAG(7),SNDPT,SNTMP
+     1AEADJ,NEXLAG,EXLAG(7),SNDPT,SNTMP,TAPREV
       COMMON/SNUP19/MFC,SFALLX,WINDC,SCTOL,WETOL,SNOF,UADJC
+C
+C    ================================= RCS keyword statements ==========
+CGW      CHARACTER*68     RCSKW1,RCSKW2
+CGW      DATA             RCSKW1,RCSKW2 /                                 '
+CGW     .$Source: /fs/hseb/ob90/ohd/ofs/src/fcst_snow/RCS/aeco19.f,v $
+CGW     . $',                                                             '
+CGW     .$Id: aeco19.f,v 1.4 2006/10/03 19:33:03 hsu Exp $
+CGW     . $' /
+C    ===================================================================
+C
 C.......................................
 C     DETERMINE IF CURRENTLY ON DEPLETION CURVE OR NEW SNOW LINE.
       TEX=0.0
@@ -37,22 +52,26 @@ C     DETERMINE IF CURRENTLY ON DEPLETION CURVE OR NEW SNOW LINE.
       IF(SWE.LE.SB) GO TO 100
 C.......................................
 C     CURRENTLY ON NEW SNOW LINE.
+C     IF COVER LE SBAESC THEN BACK ON DEPLETION CURVE
       IF (COVER.LE.SBAESC) GO TO 100
       R=(SWE/AI)*10.0+1.0
       N=R
       FN=N
       R=R-FN
       AESC=ADC(N)+(ADC(N+1)-ADC(N))*R
+C     IF COVER LE DEPLETION CURVE VALUE FOR CURRENT SWE THEN BACK
+C       ON DEPLETION CURVE
       IF(COVER.LE.AESC) GO TO 100
 C     ADJUST SBWS , LEAVE AEADJ AS IS.
       SBWS=((1.0-SBAESC)/(COVER-SBAESC))*(SWE-SB)+SB
-      IF (SBWS.GT.SB+0.75*SNOF) RETURN
-      SB=SBWS-0.75*SNOF
-      R=(SB/AI)*10.0+1.0
-      N=R
-      FN=N
-      R=R-FN
-      SBAESC=ADC(N)+(ADC(N+1)-ADC(N))*R
+CEA   FOLLOWING STATEMENTS NO LONGER NEEDED
+CEA      IF (SBWS.GT.SB+0.75*SNOF) RETURN
+CEA      SB=SBWS-0.75*SNOF
+CEA      R=(SB/AI)*10.0+1.0
+CEA      N=R
+CEA      FN=N
+CEA      R=R-FN
+CEA      SBAESC=ADC(N)+(ADC(N+1)-ADC(N))*R
       RETURN
 C.......................................
 C     CURRENTLY OR SHOULD BE ON THE DEPLETION CURVE.
@@ -69,4 +88,3 @@ C     CURRENTLY OR SHOULD BE ON THE DEPLETION CURVE.
 C.......................................
       RETURN
       END
-
